f6018a8db901902d5e2b70cca623912a41e3da93
yura <komarnickijura@gmail.com>
2023-10-20 14:41:21 +0300

    add test

--- test/main.py
+++ test/main.py
@@ -15,24 +15,56 @@ class LoginTest(unittest.TestCase):
         driver = self.driver
         driver.get("http://127.0.0.1:8000/")
 
-        # Use explicit wait to ensure elements are present
+       
         wait = WebDriverWait(driver, 10)
-        username_input = wait.until(EC.presence_of_element_located((By.NAME, "email")))
-        username_input.send_keys("yura@gmail.com")
+        time.sleep(4)
+        email_input = wait.until(EC.presence_of_element_located((By.NAME, "email")))
+        email_input.send_keys("yura@gmail.com")
 
         password_input = wait.until(EC.presence_of_element_located((By.NAME, "password")))
-        password_input.send_keys("47sifufu")
+        password_input.send_keys("yura2001")
+        
 
         login_submit_button = wait.until(EC.presence_of_element_located((By.XPATH, '//button[@type="submit"]')))
         login_submit_button.click()
+        time.sleep(4)
 
         logout_button = driver.find_element(By.LINK_TEXT, "Logout")
         logout_button.click()
-
+        
+        logout_button = driver.find_element(By.LINK_TEXT, "Logout")
+        logout_button.click()
+        
+        
+        time.sleep(4)
+        
         self.assertTrue(wait.until(EC.presence_of_element_located((By.NAME, "email"))))
+    
+    @unittest.expectedFailure
+    def test_login_not_valid_credentials(self):
+        driver = self.driver
+        driver.get("http://127.0.0.1:8000/")
+       
+        wait = WebDriverWait(driver, 10)
+        time.sleep(4)
+        email_input = wait.until(EC.presence_of_element_located((By.NAME, "email")))
+        email_input.send_keys("yura@gmail.com")
 
+        password_input = wait.until(EC.presence_of_element_located((By.NAME, "password")))
+        password_input.send_keys("yu")
+        time.sleep(4)
+        
+
+        login_submit_button = wait.until(EC.presence_of_element_located((By.XPATH, '//button[@type="submit"]')))
+        login_submit_button.click()
+        
+        self.assertTrue(wait.until(EC.presence_of_element_located((By.NAME, "Logout"))))
+        
+    
+      
+        
     def tearDown(self):
-        time.sleep(5)
+        time.sleep(4)
         self.driver.quit()
 
 if __name__ == "__main__":
